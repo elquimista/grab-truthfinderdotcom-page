6aec16c3944c2a3382b24aa59c0d38b52cfce7fe
jconsigliere <jconsigliere@gmx.com>
2017-01-13 15:31:17 +0000

    Feat: add extra fields

--- index.js
+++ index.js
@@ -13,15 +13,18 @@ const router = new Router();
 
 const { ZAPIER_WEBHOOKS_URL, TRUTHFINDER_LOGIN_EMAIL, TRUTHFINDER_LOGIN_PASSWORD } = process.env;
 
-async function enquireProfileData({ phoneNumber }) {
+async function enquireProfileData({ eventName, firstName, lastName, email, phoneNumber }) {
   const info = await csrfLogin({ email: TRUTHFINDER_LOGIN_EMAIL, password: TRUTHFINDER_LOGIN_PASSWORD });
   const data = await info.requestAsync(`/dashboard/report/phone/${phoneNumber}`, {});
-  const res = await requestify.post(ZAPIER_WEBHOOKS_URL, { rawHtml: data.body });
+  const res = await requestify.post(ZAPIER_WEBHOOKS_URL, {
+    eventName, firstName, lastName, email, phoneNumber,
+    rawHtml: data.body
+  });
   return res;
 }
 
 router.post('/', (ctx, next) => {
-  const { phoneNumber } = ctx.request.body;
+  const { eventName, firstName, lastName, email, phoneNumber } = ctx.request.body;
   if (phoneNumber) {
     enquireProfileData({ phoneNumber });
     ctx.body = {
