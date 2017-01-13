20eb587c74315ab6fd13c7f936c36a58fbe60e0b
jconsigliere <jconsigliere@gmx.com>
2017-01-13 08:01:17 +0000

    Feat: handle dynamic phoneNumber

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 'use strict';
 
+require('dotenv').config();
+
+const Koa = require('koa');
+const Router = require('koa-router');
 const csrfLogin = require('csrf-login');
 const requestify = require('requestify');
 
-require('dotenv').config();
+const app = new Koa();
+const router = new Router();
 
-async function run() {
-  const { ZAPIER_WEBHOOKS_URL, TRUTHFINDER_LOGIN_EMAIL, TRUTHFINDER_LOGIN_PASSWORD } = process.env;
+const { ZAPIER_WEBHOOKS_URL, TRUTHFINDER_LOGIN_EMAIL, TRUTHFINDER_LOGIN_PASSWORD } = process.env;
+
+router.get('/', async (ctx, next) => {
+  const { phoneNumber } = ctx.query;
   const info = await csrfLogin({ email: TRUTHFINDER_LOGIN_EMAIL, password: TRUTHFINDER_LOGIN_PASSWORD });
-  const data = await info.requestAsync('/dashboard/report/phone/8183097126', {});
-
+  const data = await info.requestAsync(`/dashboard/report/phone/${phoneNumber}`, {});
   const res = await requestify.post(ZAPIER_WEBHOOKS_URL, { rawHtml: data.body });
-  console.log(res.body);
-}
 
-run();
+  console.log(res.body);
+  ctx.body = res.body;
+  return next();
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(5000);
